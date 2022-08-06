1ddc06e78c88d0762811b4bf5e14a80d7b438747
Jasson Silva <jassonjcs11@gmail.com>
2022-08-06 13:04:59 -0300

    criação de FCFS e SSTF

---
+++ escalonamento_disco.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class Disco:
+    def __init__(self):
+        self.entrada = []
+        with open('input.txt') as file:
+            self.entrada = file.readlines()
+            file.close()
+
+        self.entrada = [int(i) for i in self.entrada]
+        self.raio = self.entrada[0]
+        self.pos_inicial = self.entrada[1]
+        self.entrada = self.entrada[2:]
+
+        self.error()
+    def error(self):
+        for i in self.entrada:
+            if i > self.raio or i < 0:
+                raise TypeError("Entrada invalida: ",i)
+    def FCFS(self):
+        distancia_total = 0
+        pos_atual = self.pos_inicial
+        for i in self.entrada:
+            distancia_percorrida = abs(i-pos_atual)
+            distancia_total += distancia_percorrida
+            pos_atual = i
+        return distancia_total
+
+    def SSTF(self):
+        distancia_total = 0
+        entrada = np.array(self.entrada)
+        pos_atual = self.pos_inicial
+        while(len(entrada)!=0):
+            diff = np.absolute(entrada-pos_atual)
+            indice = diff.argmin()
+            distancia_percorrida = abs(entrada[indice]-pos_atual)
+            distancia_total += distancia_percorrida
+            pos_atual = entrada[indice]
+            entrada = np.delete(entrada,indice)
+        return distancia_total
+
+print(Disco().FCFS())
+print(Disco().SSTF())
