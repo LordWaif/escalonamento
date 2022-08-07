c4ec9eb7778ad0471c838d62549af836f1bbf20a
Jasson Silva <jassonjcs11@gmail.com>
2022-08-07 19:31:31 -0300

    loteria consertado bug

--- escalonamento_cpu.py
+++ escalonamento_cpu.py
@@ -1,5 +1,7 @@
 import numpy as np
 import sys
+import random as rd
+
 class CPU:
     def __init__(self):
         self.entrada = []
@@ -11,7 +13,7 @@ class CPU:
             self.entrada.append([int(chegada),int(duração),1,count,int(duração)])
             count+=1
         self.entrada = np.array(self.entrada)
-        pass
+        self.copy_entrada = self.entrada.copy()
 
         self.error()
     def error(self):
@@ -35,8 +37,38 @@ class CPU:
                 RETORNO_MEDIO.append(INSTANTE - self.entrada[indice_maior,0])
                 ESPERA_MEDIA.append(INSTANTE - self.entrada[indice_maior,0] - self.entrada[indice_maior,4])
                 self.entrada = np.delete(self.entrada,indice_maior,axis=0)
-        return sum(RETORNO_MEDIO)/len(RETORNO_MEDIO),sum(RESPOSTA_MEDIA)/len(RESPOSTA_MEDIA),sum(ESPERA_MEDIA)/len(ESPERA_MEDIA)
-       
+        self.entrada = self.copy_entrada
+        return np.mean(RETORNO_MEDIO),np.mean(RESPOSTA_MEDIA),np.mean(ESPERA_MEDIA)
+    
+    def LOTERIA(self):
+        EXECUTADOS = []
+        EXCLUIDOS = []
+        RETORNO_MEDIO,RESPOSTA_MEDIA,ESPERA_MEDIA = [],[],[]
+        INSTANTE = 0
+        while(np.sum(self.entrada[:,1]) !=0):
+            escolhido = rd.randint(self.entrada[:,3][0],self.entrada[:,3][-1])
+            while(escolhido in EXCLUIDOS):
+                escolhido = rd.randint(self.entrada[:,3][0],self.entrada[:,3][-1])
+            indice = np.where(self.entrada[:,3] == escolhido)
+            if self.entrada[indice,3] not in EXECUTADOS:
+                RESPOSTA_MEDIA.append(INSTANTE-self.entrada[indice,0])
+                EXECUTADOS.append(self.entrada[indice,3])
+            # Executando
+            self.entrada[indice,1] -= 1
+            INSTANTE += 1
+            if self.entrada[indice,1] == 0:
+                RETORNO_MEDIO.append(INSTANTE - self.entrada[indice,0])
+                ESPERA_MEDIA.append(INSTANTE - self.entrada[indice,0] - self.entrada[indice,4])
+                EXCLUIDOS.append(self.entrada[indice,3])
+                self.entrada = np.delete(self.entrada,indice,axis=0)
+        self.entrada = self.copy_entrada
+        return np.mean(RETORNO_MEDIO),np.mean(RESPOSTA_MEDIA),np.mean(ESPERA_MEDIA)
+
+    def RoundRobin(self):
+        pass
+
 escalonador = CPU()
 pri = escalonador.PRI()
+lot = escalonador.LOTERIA()
 print('PRI {:.2f} {:.2f} {:.2f}'.format(pri[0],pri[1],pri[2]))
+print('LOT {:.2f} {:.2f} {:.2f}'.format(lot[0],lot[1],lot[2]))
